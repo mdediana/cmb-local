e5cd8738c0af3128c0df47bc418c25715f3846ea
Mauricio De Diana <mdediana@gmail.com>
2012-10-10 18:07:09 -0300

    Compute average users

--- plot-metrics.R
+++ plot-metrics.R
@@ -4,31 +4,31 @@ T1 <- read.table("res.1.csv", header = TRUE, sep = ",")
 T2 <- read.table("res.2.csv", header = TRUE, sep = ",")
 T3 <- read.table("res.3.csv", header = TRUE, sep = ",")
 
-ops_s <- apply(cbind(T1$ops_s, T2$ops_s, T3$ops_s), 1, mean)
-get <- apply(cbind(T1$get, T2$get, T3$get), 1, mean)
-upd <- apply(cbind(T1$upd, T2$upd, T3$upd), 1, mean)
-confl <- apply(cbind(T1$confl, T2$confl, T3$confl), 1, mean)
-mig <- apply(cbind(T1$mig, T2$mig, T3$mig), 1, mean)
-T <- cbind(T1[1:5], ops_s, get, upd, confl, mig)
-
-CS <- levels(T$consist)
-RS <- levels(T$r_w)
+CS <- levels(T1$consist)
+RS <- levels(T1$r_w)
 LS <- unique(T1$loc)
-PS <- levels(T$pop)
+PS <- levels(T1$pop)
 
 EXP <- function(x, a, b) { a * exp(b * x) }
 
-plot_fit_ops_s <- function(t) {
+avgUsers <- function(t) {
+  rw <- strsplit(RS[t$r_w], ":")
+  r <- as.numeric(sapply(rw, "[[", 1))
+  w <- as.numeric(sapply(rw, "[[", 2))
+  t$ops_s * (r * t$get + w * t$upd) # little's law
+}
+
+plotFitOps_s <- function(t) {
   fm <- nls(ops_s ~ EXP(delay, a, b), t, c(a = 30000, b = -0.005))
   curve(EXP(x, coef(fm)[1], coef(fm)[2]), add = TRUE, col = "red")
 }
 
-plot_fit_upd <- function(t) {
+plotFitUpd <- function(t) {
   fm <- lm(upd ~ delay, t)
   curve(coef(fm)[1] + coef(fm)[2] * x, add = TRUE, col = "red")
 }
 
-plot_metric <- function(t, ycolname, xcolname, fitfun = NULL) {
+plotMetric <- function(t, ycolname, xcolname, fitfun = NULL) {
   for (c in CS) {
     fname <- paste(paste(ycolname, c, sep = "_"), "png", sep = ".")
     png(fname, width = 1200, height = 1500)
@@ -40,7 +40,8 @@ plot_metric <- function(t, ycolname, xcolname, fitfun = NULL) {
           s <- subset(t, consist == c & r_w == r & loc == l & pop == p)
 
           gname <- paste(r, l, p)
-          plot(s[[ycolname]] ~ s[[xcolname]], data = s, main = gname, xlab = xcolname, ylab = ycolname)
+          plot(s[[ycolname]] ~ s[[xcolname]], data = s, main = gname,
+               xlab = xcolname, ylab = ycolname)
 
           if (!is.null(fitfun)) { fitfun(s) }
         }
@@ -50,8 +51,17 @@ plot_metric <- function(t, ycolname, xcolname, fitfun = NULL) {
   graphics.off()
 }
 
-plot_metric(T, "ops_s", "delay", plot_fit_ops_s)
-plot_metric(T, "get", "delay")
-plot_metric(T, "upd", "delay", plot_fit_upd)
-plot_metric(T, "confl", "delay")
-plot_metric(T, "mig", "delay")
+ops_s <- rowMeans(cbind(T1$ops_s, T2$ops_s, T3$ops_s))
+get <- rowMeans(cbind(T1$get, T2$get, T3$get))
+upd <- rowMeans(cbind(T1$upd, T2$upd, T3$upd))
+confl <- rowMeans(cbind(T1$confl, T2$confl, T3$confl))
+mig <- rowMeans(cbind(T1$mig, T2$mig, T3$mig))
+
+T <- cbind(T1[1:5], ops_s, get, upd, confl, mig)
+T <- cbind(T, users = avgUsers(T))
+
+plotMetric(T, "ops_s", "delay", plotFitOps_s)
+plotMetric(T, "get", "delay")
+plotMetric(T, "upd", "delay", plotFitUpd)
+plotMetric(T, "confl", "delay")
+plotMetric(T, "users", "delay")
