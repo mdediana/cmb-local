d3650bb83bc2c3c4128af4a22003b46ad14da7ed
Mauricio De Diana <mdediana@gmail.com>
2012-10-15 15:15:48 -0300

    Add migration and create a common linear regression function

--- plot-metrics.R
+++ plot-metrics.R
@@ -23,20 +23,21 @@ AvgUsers <- function(t) {
   t$ops_s * (r * t$get + w * t$upd) # little's law
 }
 
+PlotFitLin <- function(t, formula) {
+  fm <- lm(formula, t)
+  # errors: gcap 5.6.4
+  # print(summary(fm))
+  curve(coef(fm)[1] + coef(fm)[2] * x, add = TRUE, col = "red")
+}
+
 PlotFitOps_s <- function(t) {
   fm <- nls(ops_s ~ EXP(delay, a, b), t, c(a = 30000, b = -0.005))
   curve(EXP(x, coef(fm)[1], coef(fm)[2]), add = TRUE, col = "red")
 }
 
-PlotFitUpd <- function(t) {
-  fm <- lm(upd ~ delay, t)
-  curve(coef(fm)[1] + coef(fm)[2] * x, add = TRUE, col = "red")
-}
-
-PlotFitUsers <- function(t) {
-  fm <- lm(users ~ delay, t)
-  curve(coef(fm)[1] + coef(fm)[2] * x, add = TRUE, col = "red")
-}
+PlotFitGet <- function(t) { PlotFitLin(t, get ~ delay) }
+PlotFitUpd <- function(t) { PlotFitLin(t, upd ~ delay) }
+PlotFitUsers <- function(t) { PlotFitLin(t, users ~ delay) }
 
 PlotMetric <- function(t, ycolname, xcolname, ylim, fitfun = NULL) {
   for (c in CS) {
@@ -74,4 +75,5 @@ PlotMetric(T, "ops_s", "delay", c(0,3e4), PlotFitOps_s)
 PlotMetric(T, "get", "delay", c(0, 150))
 PlotMetric(T, "upd", "delay", c(0,450), PlotFitUpd)
 PlotMetric(T, "confl", "delay", c(0, 35))
+PlotMetric(T, "mig", "delay", c(0, 10))
 PlotMetric(T, "users", "delay", c(2e5, 4e6), PlotFitUsers)
