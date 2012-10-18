338789e3a9e44d5268ee1e9334e4c06014a7dba8
Mauricio De Diana <mdediana@gmail.com>
2012-10-18 17:10:43 -0300

    Refactoring

--- plot-metrics.R
+++ plot-metrics.R
@@ -1,9 +1,17 @@
 #!/usr/bin/Rscript
 
-T1 <- read.table("res.1.csv", header = TRUE, sep = ",")
-T2 <- read.table("res.2.csv", header = TRUE, sep = ",")
-T3 <- read.table("res.3.csv", header = TRUE, sep = ",")
-TS <- list(T1, T2, T3)
+#library(Hmisc)
+
+Load <- function(fn) {
+  order.by = c("consist", "r_w", "loc", "pop", "delay")
+
+  t <- read.table(fn, header = TRUE, sep = ",")
+  t <- t[do.call(order, t[order.by]), ]
+}
+
+T1 <- Load("res.1.csv")
+T2 <- Load("res.2.csv")
+T3 <- Load("res.3.csv")
 
 CS <- levels(T1$consist)
 RS <- levels(T1$r_w)
@@ -11,10 +19,40 @@ LS <- unique(T1$loc)
 PS <- levels(T1$pop)
 DS <- unique(T1$delay)
 
-EXP <- function(x, a, b) { a * exp(b * x) }
-
 Mean <- function(ts, colname) {
   rowMeans(sapply(ts, function(t) t[[colname]]))
+}
+
+Sd <- function(ts, colname) {
+  cols <- sapply(ts, function(t) t[[colname]])
+  apply(cols, 1, sd)
+}
+
+AvgUsers <- function(t) {
+  rw <- strsplit(RS[t$r_w], ":")
+  r <- as.numeric(sapply(rw, "[[", 1))
+  w <- as.numeric(sapply(rw, "[[", 2))
+  t$ops_s * (r * t$get + w * t$upd) # little's law
+}
+
+TableMean <- function(...) {
+  ts = list(...)
+
+  get <- Mean(ts, "get")
+  upd <- Mean(ts, "upd")
+  ops_s <- Mean(ts, "ops_s")
+  confl <- Mean(ts, "confl")
+  mig <- Mean(ts, "mig")
+
+  get.sd <- Sd(ts, "get")
+  upd.sd <- Sd(ts, "upd")
+  ops_s.sd <- Sd(ts, "ops_s")
+  confl.sd <- Sd(ts, "confl")
+  mig.sd <- Sd(ts, "mig")
+
+  t <- cbind(T1[1:5], ops_s, ops_s.sd, get, get.sd, upd, upd.sd, confl, confl.sd,
+             mig, mig.sd)
+  cbind(t, users = AvgUsers(t))
 }
 
 CheckFit <- function(t, fm, formula) {
@@ -32,13 +70,6 @@ CheckFit <- function(t, fm, formula) {
   }
 }
 
-AvgUsers <- function(t) {
-  rw <- strsplit(RS[t$r_w], ":")
-  r <- as.numeric(sapply(rw, "[[", 1))
-  w <- as.numeric(sapply(rw, "[[", 2))
-  t$ops_s * (r * t$get + w * t$upd) # little's law
-}
-
 PlotFitLin <- function(t, formula) {
   fm <- lm(formula, t)
   CheckFit(t, fm, formula)
@@ -54,9 +85,10 @@ PlotFitGet <- function(t) {
 }
 
 PlotFitOps_s <- function(t) {
-  fm <- nls(ops_s ~ EXP(delay, a, b), t, c(a = 30000, b = -0.005))
+  f <- function(x, a, b) { a * exp(b * x) }
+  fm <- nls(ops_s ~ f(delay, a, b), t, c(a = 30000, b = -0.005))
   # do not check fitness because r-squared is valid for linear regression only
-  curve(EXP(x, coef(fm)[1], coef(fm)[2]), add = TRUE, col = "red")
+  curve(f(x, coef(fm)[1], coef(fm)[2]), add = TRUE, col = "red")
 }
 
 PlotFitUpd <- function(t) { PlotFitLin(t, upd ~ delay) }
@@ -72,7 +104,7 @@ PlotAll <- function(t) {
   graphics.off()
 }
 
-PlotMetric <- function(t, ycolname, xcolname, ylim, fitfun = NULL) {
+PlotMetric <- function(t, means, ycolname, xcolname, ylim, fitfun = NULL) {
   for (c in CS) {
     fname <- paste(paste(ycolname, c, sep = "_"), "png", sep = ".")
     png(fname, width = 1200, height = 1500)
@@ -81,12 +113,18 @@ PlotMetric <- function(t, ycolname, xcolname, ylim, fitfun = NULL) {
     for (r in RS) {
       for (l in LS) {
         for (p in PS) {
-          s <- subset(t, consist == c & r_w == r & loc == l & pop == p)
+          s <- subset(means, consist == c & r_w == r & loc == l & pop == p)
 
           gname <- paste(r, l, p)
           plot(s[[ycolname]] ~ s[[xcolname]], data = s, main = gname,
                xlab = xcolname, ylab = ycolname, ylim = ylim)
 
+#          sdcolname <- paste(ycolname, "sd", sep = ".")
+#          sdplus <- s[[ycolname]] + s[[sdcolname]]
+#          sdminus <- s[[ycolname]] - s[[sdcolname]]
+#          errbar(s[[xcolname]], s[[ycolname]], sdplus, sdminus)
+
+          s <- subset(t, consist == c & r_w == r & loc == l & pop == p)
           if (!is.null(fitfun)) { fitfun(s) }
         }
       }
@@ -95,22 +133,15 @@ PlotMetric <- function(t, ycolname, xcolname, ylim, fitfun = NULL) {
   graphics.off()
 }
 
-get <- Mean(TS, "get")
-upd <- Mean(TS, "upd")
-ops_s <- Mean(TS, "ops_s")
-confl <- Mean(TS, "confl")
-mig <- Mean(TS, "mig")
+t <- rbind(T1, T2, T3)
+means <- TableMean(T1, T2, T3)
 
-T <- cbind(T1[1:5], ops_s, get, upd, confl, mig)
-T <- cbind(T, users = AvgUsers(T))
+PlotAll(t)
 
-PlotAll(T)
-
-PlotMetric(T, "get", "delay", c(0, 20), PlotFitGet)
-#PlotMetric(T, "get", "delay", c(0, 200), PlotFitGet)
-PlotMetric(T, "upd", "delay", c(0,450), PlotFitUpd)
-PlotMetric(T, "ops_s", "delay", c(0,3e4), PlotFitOps_s)
-PlotMetric(T, "confl", "delay", c(0, 35))
-PlotMetric(T, "mig", "delay", c(0, 10))
-#PlotMetric(T, "users", "delay", c(2e5, 4e6), PlotFitUsers)
-PlotMetric(T, "users", "delay", c(2e5, 4e6))
+PlotMetric(t, means, "get", "delay", c(0, 20), PlotFitGet)
+PlotMetric(t, means, "upd", "delay", c(0,450), PlotFitUpd)
+PlotMetric(t, means, "ops_s", "delay", c(0,3e4), PlotFitOps_s)
+PlotMetric(t, means, "confl", "delay", c(0, 35))
+PlotMetric(t, means, "mig", "delay", c(0, 10))
+#PlotMetric(t, means, "users", "delay", c(2e5, 4e6), PlotFitUsers)
+PlotMetric(t, means, "users", "delay", c(2e5, 4e6))
