2e82b91ea7ae774467811ae7ae0edfe5d3648c95
Mauricio De Diana <mdediana@gmail.com>
2012-10-23 23:23:24 -0200

    New graphs and some fixes

--- plot-metrics.R
+++ plot-metrics.R
@@ -24,7 +24,8 @@ COLORS[CS[3]] <- "green"
 CheckCv <- function(m) {
   limits <- c(0.2, 0.3, 0.4, 0.5)
   for (col in c("get.cv", "upd.cv", "ops_s.cv")) {
-    cvs <- sapply(limits, function(l) dim(m[m[[col]] > l, ])[1])
+    cvs <- sapply(limits, function(l) dim(m[!is.nan(m[[col]]) &
+                                            m[[col]] > l, ])[1])
     cat(col, ":", cvs, "\n")
   }
 }
@@ -34,8 +35,9 @@ CheckFit <- function(t, fm) {
     return(NULL)
 
   # errors: gcap 5.6.4
-  r2 <- summary(fm)$adj.r.squared 
-  if (r2 < 0.7) {
+  r2 <- summary(fm)$adj.r.squared
+  # r2 may be NaN when conflicts == 0 
+  if (!is.nan(r2) && r2 < 0.7) {
     scenario <- paste(CS[t$consist[1]], RS[t$r_w[1]], t$loc[1], PS[t$pop[1]])
     cat("Bad R2:", r2, "[", all.vars(formula(fm)), "] [", scenario, "]\n")
   }
@@ -55,20 +57,36 @@ PlotFitUpd <- function(t, color = "red") {
 }
 
 PlotFitOps_s <- function(t, color = "red") {
-  f <- function(x, a, b) { a * exp(b * x) }
-  fm <- nls(ops_s ~ f(delay, a, b), t, c(a = 30000, b = -0.005))
-  # do not check fitness because r-squared is valid for linear regression only
-  curve(f(x, coef(fm)[1], coef(fm)[2]), add = TRUE, col = color)
+  if (RS[t$r_w[1]] == "1:0") {
+    fm <- lm(ops_s ~ poly(delay, 3, raw = TRUE), t)
+    CheckFit(t, fm)
+    curve(coef(fm)[1] + coef(fm)[2] * x + coef(fm)[3] * x ^ 2 +
+          coef(fm)[4] * x ^ 3, add = TRUE, col = color)
+  } else {
+    f <- function(x, a, b) { a * exp(b * x) }
+    fm <- nls(ops_s ~ f(delay, a, b), t, c(a = 30000, b = -0.005))
+    # don't check fitness because r-squared is valid for linear regression only
+    curve(f(x, coef(fm)[1], coef(fm)[2]), add = TRUE, col = color)
+  }
+}
+
+PlotFitConfl <- function(t, color = "red") {
+  fm <- lm(confl ~ poly(delay, 2, raw = TRUE), t)
+  CheckFit(t, fm)
+  curve(coef(fm)[1] + coef(fm)[2] * x + coef(fm)[3] * x ^ 2,
+        add = TRUE, col = color)
 }
 
 PlotAll <- function(t) {
-  for (d in DS) {
-    fname <- paste(paste("all", d, sep = "_"), "png", sep = ".")
-    png(fname, width = 1200, height = 1500)
-    s <- subset(t, delay == d)
-    plot(~ get + upd + ops_s + confl + mig, data = s)
+  for (c in CS) {
+    for (d in c(0, 150, 300)) {
+      fname <- paste(paste("all", c, d, sep = "_"), "png", sep = ".")
+      png(fname, width = 1200, height = 1500)
+      s <- subset(t, delay == d)
+      plot(~ get + upd + ops_s + confl + mig, data = s)
+    }
+    graphics.off()
   }
-  graphics.off()
 }
 
 PlotMetric <- function(t, ycolname, xcolname, ylim, fitfun = NULL) {
@@ -126,43 +144,73 @@ PlotConsistComp <- function(t, m, ycolname, xcolname, ylim, fitfun = NULL) {
   graphics.off()
 }
 
-AvgUsers <- function(t) {
-  rw <- strsplit(RS[t$r_w], ":")
-  r <- as.numeric(sapply(rw, "[[", 1))
-  w <- as.numeric(sapply(rw, "[[", 2))
-  t$ops_s * (r * t$get + w * t$upd) # little's law
-}
-
 metrics <- c("get", "upd", "ops_s", "confl", "mig")
-m <- ts[[1]][1:5]
-for (mn in metrics) {
-  cols <- sapply(ts, "[[", mn)
-  mean <- rowMeans(cols)
-  sd <- apply(cols, 1, sd)
-  cv <- sd / mean
-  m <- cbind(m, mean, sd, cv,
-             apply(cols, 1, function(x) sqrt(var(x) / length(x)))) # std err
+m <- data.frame()
+for (c in CS) {
+  for (r in RS)
+    for (l in LS)
+      for (p in PS)
+        for (d in DS) {
+          # 1 - remove the most distant point from the mean in each scenario
+          s <- subset(t, consist == c & r_w == r & loc == l &
+                         pop == p & delay == d)
+          get.mean <- mean(s$get)
+          upd.mean <- mean(s$upd)
+          get.cv <- sd(s$get) / get.mean
+          upd.cv <- sd(s$upd) / upd.mean
+          index <- function(v, mean) which(abs(v - mean) == max(abs(v - mean)))
+          if (is.nan(upd.cv) || get.cv > upd.cv)
+            s.i <- index(s$get, get.mean)
+          else
+            s.i <- index(s$upd, upd.mean)
+          # length(index) > 1 if more than one of some repeated value
+          # satify the previous condition, so we take the first one
+          t.rowname <- rownames(s[s.i[1], ])
+          t.i <- which(rownames(t) == t.rowname)
+          t <- t[-t.i, ]
+
+          # 2 - compute the final mean, sd, se and cv
+          s <- subset(t, consist == c & r_w == r & loc == l &
+                         pop == p & delay == d)
+          row <- s[1, 1:5]
+          for (mn in metrics) {
+            metric.mean <- mean(s[[mn]])
+            metric.sd <- sd(s[[mn]])
+            metric.se <- sqrt(var(s[[mn]]) / length(s[[mn]]))
+            metric.cv <- metric.sd / metric.mean
+            row <- cbind(row, metric.mean, metric.sd, metric.se, metric.cv)
+          }
+          m <- rbind(m, row)
+        }
 }
 
+# add colnames to the metrics df
 added.colnames <- sapply(metrics, function(mn) c(mn,
                                                  paste(mn, "sd", sep = "."),
-                                                 paste(mn, "cv", sep = "."),
-                                                 paste(mn, "se", sep = ".")))
+                                                 paste(mn, "se", sep = "."),
+                                                 paste(mn, "cv", sep = ".")))
 colnames(m) <- c(colnames(m)[1:5], c(added.colnames))
 
-m <- cbind(m, users = AvgUsers(m))
+# avg users
+rw <- strsplit(RS[m$r_w], ":")
+r <- as.numeric(sapply(rw, "[[", 1))
+w <- as.numeric(sapply(rw, "[[", 2))
+avg.users <- m$ops_s * (r * m$get + w * m$upd) # little's law
+m <- cbind(m, avg.users)
 
 CheckCv(m)
 
 PlotAll(t)
 
-PlotMetric(t, "get", "delay", c(0, 20), PlotFitGet)
-PlotMetric(t, "upd", "delay", c(0,450), PlotFitUpd)
+PlotMetric(t, "get", "delay", c(0, 2e-2), PlotFitGet)
+PlotMetric(t, "upd", "delay", c(0,4.5e-1), PlotFitUpd)
 PlotMetric(t, "ops_s", "delay", c(0,3e4), PlotFitOps_s)
-PlotMetric(t, "confl", "delay", c(0, 35))
-PlotMetric(t, "mig", "delay", c(0, 10))
-PlotMetric(m, "users", "delay", c(2e5, 4e6))
+PlotMetric(t, "confl", "delay", c(0, 1.5e-1), PlotFitConfl)
+PlotMetric(t, "mig", "delay", c(0, 1e-2))
+PlotMetric(m, "avg.users", "delay", c(2.5e2, 4e3))
 
-PlotConsistComp(t, m, "get", "delay", c(0, 20), PlotFitGet)
-PlotConsistComp(t, m, "upd", "delay", c(0,450), PlotFitUpd)
+PlotConsistComp(t, m, "get", "delay", c(0, 2e-2), PlotFitGet)
+PlotConsistComp(t, m, "upd", "delay", c(0, 4.5e-1), PlotFitUpd)
 PlotConsistComp(t, m, "ops_s", "delay", c(0,3e4), PlotFitOps_s)
+PlotConsistComp(t, m, "confl", "delay", c(0,2e-1), PlotFitConfl)
+warnings()
