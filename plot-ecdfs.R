a0d6f6c01c991b710a2dae9e4b87ad6de7153e4d
Mauricio De Diana <mdediana@gmail.com>
2012-12-12 21:56:09 -0200

    Minor changes

--- plot-ecdfs.R
+++ plot-ecdfs.R
@@ -7,7 +7,7 @@ t <- t[with(t, order(consist, r_w, loc, pop, delay)), ]
 
 for (c in levels(t$consist)) {
   for (o in levels(t$op)) {
-    fname <- paste(paste("hist", c, o, sep = "_"), "png", sep = ".")
+    fname <- paste(paste("ecdf", c, o, sep = "_"), "png", sep = ".")
     png(fname, width = 1200, height = 1500)
     par(mfrow = c(4, 4))
  
@@ -16,11 +16,13 @@ for (c in levels(t$consist)) {
           function(row) {
             gname <- paste(row[2], row[3], row[4], row[5], row[6])
 
-            s <- row[-c(1:6)]
-            s <- as.numeric(s) / 1e3	# us -> ms
+            s <- as.numeric(row[-c(1:6)])
 
             plot.ecdf(data.matrix(s), main = gname, cex = 0.2, col = "red",
-                      xlim = c(0, 1.5e3), xlab = paste(o, " (ms)"))
+                      xlim = c(0, 1.5), xlab = paste(o, " (s)"))
+            delay = as.numeric(row[5]) / 1e3	# ms -> s
+            abline(v = delay, lty = "dotted")
+            abline(h = row[3], lty = "dotted")
             #hist(t(s)[, 1], main = gname, col = "lightblue")
         })
     graphics.off()
