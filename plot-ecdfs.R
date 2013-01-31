39165e8ea61ee73b8d21261d1852135eeccce04c
Mauricio De Diana <mdediana@gmail.com>
2013-01-30 22:50:03 -0200

    Prequenas modificações

--- plot-ecdfs.R
+++ plot-ecdfs.R
@@ -7,7 +7,7 @@ t <- t[with(t, order(consist, op, loc, delay, delay_var, loss)), ]
 
 cs <- levels(t$consist)
 cols <- hash(cs, c("red", "blue", "green", "orange"))
-ltys <- hash(cs, c("solid", "dashed", "dotted", "dotdash"))
+pchs <- hash(cs, c(1, 2, 5, 6))
 
 for (c in cs)
   cat(c, cols[[c]], '\n')
@@ -27,19 +27,21 @@ for (d in unique(t$delay)) {
 
     s <- subset(t, delay_var != 0 & loss == 0 &
                    delay == d & op == o & loc == l)
-    gname <- paste(OPS.PT[[o]], "/ Loc =", l)
+    gname <- paste(OPS.PT[[o]], "/ Loc =", 100 * l, "%")
 
     u <- subset(s, consist == "ev1")
     v <- as.numeric(u[-c(1:10)])
     plot.ecdf(data.matrix(v), main = gname,
               xlim = c(0, 1.5),
               xlab = "Tempo de resposta (em s)",
-              cex = 0, cex.lab = 1.7, cex.axis = 1.3, cex.main = 1.7,
-              col = cols[[c]])
+              cex.lab = 1.7, cex.axis = 1.3, cex.main = 1.7,
+              pch = pchs[[c]], col = cols[[c]])
+    #legend(1, 0.4, c(cs), col = cols)
+
     for (c in c("ev2", "any", "lat")) {
       u <- subset(s, consist == c)
       v <- as.numeric(u[-c(1:10)])
-      plot.ecdf(data.matrix(v), col = cols[[c]], add = T)
+      plot.ecdf(data.matrix(v), pch = pchs[[c]], col = cols[[c]], add = T)
     }
 
     delay = as.numeric(s$delay) / 1e3	# ms -> s
