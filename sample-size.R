eaff5a393fe66a3544ade0e8d88dc5ef2fc3b7c8
Mauricio De Diana <mdediana@gmail.com>
2013-02-02 19:18:10 -0200

    Fix bugs

--- sample-size.R
+++ sample-size.R
@@ -3,17 +3,17 @@
 library(hash)
 
 t <- read.csv("res.1/summary.csv", header = T)
-t <- t[with(t, order(consist, op, loc, delay, delay_var)), ]
+t <- t[with(t, order(consist, loc, delay, delay_var)), ]
 
 # http://www.rit.edu/~w-uphysi/uncertainties/Uncertaintiespart2.html
 # http://www.fas.harvard.edu/~scphys/courses/ps2/2007/errprop.pdf
 op.mean <- (2 * t$get + t$upd) / 3  # ratio = 2:1
-op.sdev <- ((2 * t1$get_sdev / 3) ^ 2 + (t1$upd_sdev / 3) ^ 2) ^ 0.5
+op.sdev <- ((2 * t$get_sdev / 3) ^ 2 + (t$upd_sdev / 3) ^ 2) ^ 0.5
 
 z.90 <- 1.645 # 90%
 z.95 <- 1.960 # 95%
 z.99 <- 2.576 # 99%
-r <- 3
+r <- 1
 sample.n <- ((100 * z.99 * op.mean) / (r * op.sdev)) ^ 2
 enough <- (t$get_n + t$upd_n) > sample.n
 
