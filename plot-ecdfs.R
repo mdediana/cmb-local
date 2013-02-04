71b59065db8ab4990463efe39a10d52e2f6f8e10
Mauricio De Diana <mdediana@gmail.com>
2013-02-04 16:05:30 -0200

    Plot zoomed ECDF

--- plot-ecdfs.R
+++ plot-ecdfs.R
@@ -3,7 +3,7 @@
 library(hash)
 
 t <- read.table("res.1/percentiles.csv", header = T, sep = ",")
-t <- t[with(t, order(consist, op, loc, delay, delay_var, loss)), ]
+t <- t[with(t, order(consist, op, loc, delay, delay_var)), ]
 
 cs <- levels(t$consist)
 cols <- hash(cs, c("red", "blue", "green", "orange"))
@@ -16,6 +16,7 @@ OPS.PT <- hash()
 OPS.PT["get"] <- "Leituras"
 OPS.PT["upd"] <- "Escritas"
 
+# Ecdfs
 for (d in unique(t$delay)) {
   fname <- paste(paste("ecdf", d, sep = ''), "png", sep = ".")
   png(fname, width = 1200, height = 1500)
@@ -25,8 +26,7 @@ for (d in unique(t$delay)) {
   for (l in unique(t$loc)) {
     cat(d, o, l, '\n')
 
-    s <- subset(t, delay_var != 0 & loss == 0 &
-                   delay == d & op == o & loc == l)
+    s <- subset(t, delay_var != 0 & delay == d & op == o & loc == l)
     gname <- paste(OPS.PT[[o]], "/ Loc =", 100 * l, "%")
 
     u <- subset(s, consist == "ev1")
@@ -49,4 +49,41 @@ for (d in unique(t$delay)) {
     abline(h = l, lty = "dotted")
   }
   graphics.off()
+}
+
+# Zoom
+fname <- "ecdf_zoom.png"
+png(fname, width = 600, height = 750)
+#par(mfrow = c(2, 2))
+
+o <- "get"
+l <- 0.5
+s <- subset(t, delay_var != 0 & delay == 0 & op == o & loc == l)
+gname <- paste(OPS.PT[[o]], "/ Loc =", 100 * l, "%")
+
+u <- subset(s, consist == "ev1")
+v <- as.numeric(u[-c(1:10)])
+plot.ecdf(data.matrix(v), main = gname,
+          xlim = c(0, 0.05),
+          xlab = "Tempo de resposta (em ms)",
+          cex.lab = 1.7, cex.axis = 1.3, cex.main = 1.7,
+          pch = pchs[[c]], col = cols[[c]])
+
+graphics.off()
+
+# Relative gains
+for (l in unique(t$loc)) {
+  cat("loc:", l, '\n')
+
+  s <- subset(t, delay == 200 & delay_var == 60 & op == "upd" & loc == l)
+  ev2 <-subset(s, consist == "ev2")
+  lat <-subset(s, consist == "lat")
+  any <-subset(s, consist == "any")
+
+  cat("p75, any, lat, ev2:", any$p75, lat$p75, ev2$p75, '\n')
+  cat("p75, any / ev2:", (ev2$p75 - any$p75) / ev2$p75, '\n') 
+  cat("p75, lat / ev2:", (ev2$p75 - lat$p75) / ev2$p75, '\n') 
+  cat("p95, any, lat, ev2:", any$p95, lat$p95, ev2$p95, '\n')
+  cat("p95, any / ev2:", (ev2$p95 - any$p95) / ev2$p95, '\n') 
+  cat("p95, lat / ev2:", (ev2$p95 - lat$p95) / ev2$p95, '\n') 
 }
